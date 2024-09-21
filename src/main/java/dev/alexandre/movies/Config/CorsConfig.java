6c7c5c9556605a32e1b6aebc6d937a4e2701d3ef
coder-gaia <alexandregaia911@gmail.com>
2024-09-21 18:00:40 -0300

    Adding some configs

--- src/main/java/dev/alexandre/movies/Config/CorsConfig.java
+++ src/main/java/dev/alexandre/movies/Config/CorsConfig.java
@@ -1,6 +1,6 @@
-package dev.alexandre.movies.config;
+package dev.alexandre.movies.Config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,12 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${frontend.url)")
+    private String frontendUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/v1/**")
-                .allowedOrigins("https://golden-movies.onrender.com")
+        registry.addMapping("/api/v1/movies/**")
+                .allowedOrigins(frontendUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 }
